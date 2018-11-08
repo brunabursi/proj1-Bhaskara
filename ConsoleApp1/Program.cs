1371898f25be0e81f44996adc2865dec20f40823
Bruna Bursi dos Santos <bruna.bursi@verity.com.br>
2018-11-08 14:49:38 -0200

    projeto c# bhaskara

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, delta, x1, x2;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            a = double.Parse(vet[0]);
+            b = double.Parse(vet[1]);
+            c = double.Parse(vet[2]);
+
+            delta = b * b - 4 * a * c;
+
+            if (a == 0 || delta < 0) {
+                Console.WriteLine("Impossível calcular");
+                    } else {
+
+                x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+                x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+                Console.WriteLine("X1 = " + x1.ToString("F4"));
+                Console.WriteLine("X2 = " + x2.ToString("F4"));
+
+            } Console.ReadLine();                        
+
+
+        }
+    }
+}
